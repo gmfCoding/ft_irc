45042d20e40138b70ca892850e47f4b5662ca931
Christopher Lovell <chris.lovellmail@gmail.com>
2024-05-24 17:32:16 +0930

    Designing classes

--- inc/IRCServer.hpp
+++ inc/IRCServer.hpp
@@ -1,4 +1,4 @@
-
+#include <string>
 
 class IRCServer
 {
@@ -11,4 +11,46 @@ private:
 public:
     IRCServer(char *port, char *password);
     int Run();
+
+    int Execute(User user, Command *command, CommandParameter params);
 };
+
+class User
+{
+
+};
+
+class Client
+{
+    User user;
+};
+
+class CommandParameter
+{
+private:
+    IRCServer& m_context;
+
+public:
+    CommandParameter(IRCServer& context) : m_context(context) {}
+    ~CommandParameter() {}
+
+    int         Count();
+    std::string ReadString();
+    int         ReadInt();
+    User        ReadUser();
+};  
+
+class Command
+{
+    virtual int Run(User user, CommandParameter param) = 0;
+};
+
+class NickCommand : public Command
+{
+    int Run(User user, CommandParameter param);
+};
+
+int NickCommand::Run(User user, CommandParameter param)
+{
+
+}
