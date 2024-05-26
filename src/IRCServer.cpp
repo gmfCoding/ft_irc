acafec8a89d7ad902a03fff08654610ae0da2bee
kyle <kylemordaunt@gmail.com>
2024-05-26 22:28:38 +0930

    basic irc setup using vector array to pollFds and nonBlovking with fcntl()

--- src/IRCServer.cpp
+++ src/IRCServer.cpp
@@ -1,12 +1,31 @@
 #include "IRCServer.hpp"
 
 
-IRCServer::IRCServer(char *port, char *password) : port(port), password(password)
+IRCServer::IRCServer(int port, char *password) : _port(port), _password(password)
 {
-
+	serverSetup();
 }
 
 int IRCServer::Run()
 {
 	return (0);
+}
+
+void IRCServer::serverSetup()
+{
+    serverFd = socket(AF_INET, SOCK_STREAM, 0);
+    int opt = 1;
+    if (serverFd == -1)
+		throw std::runtime_error("Failed to create socket");
+    if (setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+		throw std::runtime_error("setsockopt failed");
+	fcntl(serverFd, F_SETFL, O_NONBLOCK);
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(_port);
+    if (bind(serverFd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
+		throw std::runtime_error("Bind failed");
+	if (listen(serverFd, SOMAXCONN) == -1)
+		throw std::runtime_error("Listen failed");
+    pollFds.push_back({serverFd, POLLIN, 0});
 }
