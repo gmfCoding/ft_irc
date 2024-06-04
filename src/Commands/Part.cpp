4c239382a7ba9ef068d3ad8fb7d9937f808a1d70
kyle <kylemordaunt@gmail.com>
2024-06-05 00:05:00 +0930

    implemered PART so you can now leave channels

--- src/Commands/Part.cpp
+++ src/Commands/Part.cpp
@@ -2,9 +2,32 @@
 
 void Command::handlePartCommand(IRCClient* client, const std::vector<std::string>& parameters)
 {
-    if (parameters.empty())
-    {
-        // send error message to client about missing channel name
-        return ;
-    }
+	if (parameters.empty())
+	{
+	    client->GetServer()->clientSendData(client->GetFd(), ERR_NEEDMOREPARAMS(client->GetNickname(), "PART"));
+	    return;
+	}
+	std::string channelName = parameters[0];
+	IRCChannel* channel = client->GetServer()->GetChannel(channelName);
+	if (!channel)
+	{
+	    client->GetServer()->clientSendData(client->GetFd(), ERR_NOSUCHCHANNEL(client->GetNickname(), channelName));
+	    return;
+	}
+	if (!channel->isMember(client))
+	{
+	    client->GetServer()->clientSendData(client->GetFd(), ERR_NOTONCHANNEL(client->GetNickname(), channelName));
+	    return;
+	}
+	channel->removeMember(client);
+	client->SetCurrentChannel(nullptr);
+	std::string partMessage = ":" + client->GetNickname() + " PART " + channelName;
+	channel->broadcast(partMessage);
+	client->GetServer()->clientSendData(client->GetFd(), partMessage);
+	
+//	if (channel->GetMemberList().empty())//TODO: make decision if the channel is removed when there are no more members left
+//	{
+//	    client->GetServer()->removeChannel(channelName);//need to add a removeChannel fucntion
+//	    delete channel;
+//	}
 }
